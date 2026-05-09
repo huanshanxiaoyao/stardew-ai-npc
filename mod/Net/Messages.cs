e6743f3689f7cb882ef8190abe165bac168acf9b
Jack <278171810@qq.com>
2026-05-09 16:28:18 +0800

    feat(mod): wire-protocol record types

---
+++ mod/Net/Messages.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace StardewAiMod.Net
+{
+    public record NpcInteract(
+        [property: JsonPropertyName("id")] string Id,
+        [property: JsonPropertyName("npc")] string Npc,
+        [property: JsonPropertyName("player")] string Player,
+        [property: JsonPropertyName("location")] string Location,
+        [property: JsonPropertyName("ts")] long Ts
+    )
+    {
+        [JsonPropertyName("type")]
+        public string Type => "npc_interact";
+
+        [JsonPropertyName("v")]
+        public int V => 1;
+    }
+
+    public record NpcReply(
+        [property: JsonPropertyName("id")] string Id,
+        [property: JsonPropertyName("npc")] string Npc,
+        [property: JsonPropertyName("text")] string Text,
+        [property: JsonPropertyName("done")] bool Done
+    )
+    {
+        [JsonPropertyName("type")]
+        public string Type => "npc_reply";
+
+        [JsonPropertyName("v")]
+        public int V => 1;
+    }
+
+    public record SessionReset(
+        [property: JsonPropertyName("reason")] string Reason
+    )
+    {
+        [JsonPropertyName("type")]
+        public string Type => "session_reset";
+
+        [JsonPropertyName("v")]
+        public int V => 1;
+    }
+}
